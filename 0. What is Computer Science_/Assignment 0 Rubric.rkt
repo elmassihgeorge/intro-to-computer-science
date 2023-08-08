1b889366818488bbd331b07e279f2bdc07e81ab6
elmassihgeorge <33693153+elmassihgeorge@users.noreply.github.com>
2023-08-08 07:49:30 -0400

    Add files via upload

---
+++ 0. What is Computer Science_/Assignment 0 Rubric.rkt
@@ -0,0 +1,64 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname |Assignment 0 Rubric|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+; Assignment 0
+; Welcome to Racket!
+
+; "Comments" are lines of code that don't get executed -
+; they're great for leaving notes like this in the margins!
+
+; To make a single line comment, we use a semicolon like this
+
+#|
+We can also make multi line comments
+if we want to include larger chunks of text
+in our code. We do this by starting the block with
+a pound sign and straight line (shift + back slash),
+and end the chunk with a straight line then pound sign.
+|#
+
+; Hello, my name is George
+
+#|
+I can write
+many lines
+in this chunk
+of comments!
+|#
+
+; Problem 1:
+; On a single line comment below, write your name, grade, and where you're from!
+
+; +1 point for name, +1 point for grade, +1 point for from all in a single line comment
+
+; Problem 2:
+; In a multi-line comment, define computer science in your own words.
+
+#|
++3 points for a basic definition
+of computer science; the science
+of programming computers, solving
+problems, creating software etc.
+|#
+
+; Problem 3:
+; In a multi-line comment, write who Alan Turing was and why he's important to computer science
+
+#|
++3 points for some form
+of saying that Alan Turing
+is the father of modern
+computer science.
+|#
+
+; Problem 4:
+; In a multi-line comment, write why you're interested in computer science
+; and one specific skill in programming you'd like to learn
+
+#|
++3 points for any
+response involving
+why they want to learn
+computer science and
+what skill they want to learn.
+|#
